ed494473852bda266e7b6e9f903fcd2d76e3015a
Kinka <katyamew@gmail.com>
2023-05-18 04:43:27 +1000

    _

--- auth/model/User.js
+++ auth/model/User.js
@@ -25,7 +25,7 @@ const User = new Schema({
     },
     registrDate:{
         type: Number,
-        default: new Date.now(),
+        default: Date.now(),
         required: true
     },
     age:{
