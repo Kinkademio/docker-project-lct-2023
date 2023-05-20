9050f03729079f1bc84a307f85d949430d5175c2
Kinka <katyamew@gmail.com>
2023-05-21 04:31:45 +1000

    Try to fix webSoket

--- clients/src/boot/axios.js
+++ clients/src/boot/axios.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: 'http://graduate-map.ru/'
+  baseURL: 'https://graduate-map.ru/'
 })
 
 export default boot(({
