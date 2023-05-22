6e5b8a092816da3ef29e6bf8872306815bc8e3f6
Ximanitt <Maximka.kotic@yandex.ru>
2023-05-22 12:20:01 +1000

    Вставил регулярку на смену пароля юзера

--- clients/src/pages/Profile.vue
+++ clients/src/pages/Profile.vue
@@ -6,20 +6,24 @@
           <div class="text-h6">Данные пользователя</div>
         </q-card-section>
         <q-card-section>
-
-          <q-input :readonly="cantEdit" v-model="name" hint="Имя пользователя" @blur="changeName()">
+          <q-input
+            :readonly="cantEdit"
+            v-model="name"
+            hint="Имя пользователя"
+            @blur="changeName()"
+          >
             <template v-slot:append>
-              <q-icon :name="'edit'" class="cursor-pointer" @click="cantEdit = !cantEdit">
+              <q-icon
+                :name="'edit'"
+                class="cursor-pointer"
+                @click="cantEdit = !cantEdit"
+              >
               </q-icon>
             </template>
           </q-input>
 
-
           <q-input :readonly="true" v-model="username" hint="Логин в системе" />
-
-
         </q-card-section>
-
       </div>
       <q-separator vertical inset></q-separator>
       <div class="col">
@@ -27,25 +31,39 @@
           <div class="text-h6">Изменение пароля</div>
         </q-card-section>
         <q-card-section>
-
-          <q-input v-model="password" :type="isPwd ? 'password' : 'text'" label="Новый пароль">
+          <q-input
+            v-model="password"
+            :type="isPwd ? 'password' : 'text'"
+            label="Новый пароль"
+          >
             <template v-slot:append>
-              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
-                @click="isPwd = !isPwd"></q-icon>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              ></q-icon>
             </template>
           </q-input>
         </q-card-section>
 
         <q-card-section>
-          <q-input v-model="password_again" :type="isPwd ? 'password' : 'text'" label="Подтвержение пароля" />
+          <q-input
+            v-model="password_again"
+            :type="isPwd ? 'password' : 'text'"
+            label="Подтвержение пароля"
+          />
         </q-card-section>
 
         <q-card-section>
-          <q-btn flat class="button" label="Изменить пароль" @click="changeUserPass"></q-btn>
+          <q-btn
+            flat
+            class="button"
+            label="Изменить пароль"
+            @click="changeUserPass"
+          ></q-btn>
         </q-card-section>
       </div>
     </div>
-
   </q-card>
 </template>
 
@@ -55,8 +73,8 @@ import { ref } from "vue";
 import VueCookies from "vue-cookies";
 export default {
   beforeMount() {
-    this.username = VueCookies.get('login');
-    this.name = VueCookies.get('uname');
+    this.username = VueCookies.get("login");
+    this.name = VueCookies.get("uname");
   },
   data() {
     return {
@@ -65,7 +83,7 @@ export default {
       isPwd: ref(true),
       username: "",
       name: "",
-      cantEdit: true,
+      cantEdit: true
     };
   },
   methods: {
@@ -73,30 +91,30 @@ export default {
       this.cantEdit = true;
       if (this.name == "") {
         this.$q.notify({
-          type: 'negative',
+          type: "negative",
           message: "Имя пользователя не может быть пустым"
         });
         return;
       }
-      if (VueCookies.get('uname') == this.name) {
+      if (VueCookies.get("uname") == this.name) {
         return;
       }
       try {
         const res = await api.post("auth/user", {
-          username: VueCookies.get('login'),
+          username: VueCookies.get("login")
         });
 
         const response = await api.put("auth/updateUser/" + res.data._id, {
-          name: this.name,
+          name: this.name
         });
         this.$q.notify({
-          type: 'positive',
+          type: "positive",
           message: "Имя пользователя изменено"
         });
-        VueCookies.set('uname', this.name, "10h");
+        VueCookies.set("uname", this.name, "10h");
         window.location.reload();
       } catch (error) {
-        this.name = VueCookies.get('uname');
+        this.name = VueCookies.get("uname");
         this.onError(error);
       }
     },
@@ -104,7 +122,7 @@ export default {
       this.loaded = true;
       if (!error.response || !error.response.status) {
         this.$q.notify({
-          type: 'negative',
+          type: "negative",
           message: "Нет соединения с сервером"
         });
         return;
@@ -112,57 +130,65 @@ export default {
       this.error = error.response.status;
       if (this.error != 403) {
         this.$q.notify({
-          type: 'negative',
-          message: error.response.data.message ?? 'Ошибка сервера'
+          type: "negative",
+          message: error.response.data.message ?? "Ошибка сервера"
         });
       }
     },
     async changeUserPass() {
       if (this.password == "" || this.password < 4) {
         this.$q.notify({
-          type: 'negative',
+          type: "negative",
           message: "Новый пароль должен быть более 3 символов"
         });
         return;
       }
       if (this.password_again == "") {
         this.$q.notify({
-          type: 'negative',
+          type: "negative",
           message: "Введите подтверждение пароля"
+        });
+        return;
+      }
+      const passwordRegex = /^[a-zA-Z._]+$/;
+      if (!passwordRegex.test(this.password)) {
+        this.$q.notify({
+          type: "negative",
+          message:
+            "Пароль должен содержать только буквы a-z, A-Z, точку (.) и символ подчеркивания (_)"
         });
         return;
       }
       if (this.password_again != this.password) {
         this.$q.notify({
-          type: 'negative',
+          type: "negative",
           message: "Пароли не совпадают"
         });
         return;
       }
       try {
         const res = await api.post("auth/user", {
-          username: VueCookies.get('login'),
+          username: VueCookies.get("login")
         });
 
         await api.put("auth/updateUser/" + res.data._id, {
-          password: this.password,
+          password: this.password
         });
         this.loaded = true;
         this.$q.notify({
-          type: 'positive',
+          type: "positive",
           message: "Пароль изменен"
         });
       } catch (error) {
         this.onError(error);
       }
-    },
-  },
-
+    }
+  }
 };
 </script>
 <style scoped>
 .button {
-  background-color: rgba(7, 7, 7, 0.050);
+  background-color: rgba(7, 7, 7, 0.05);
   width: 100%;
 }
 </style>
