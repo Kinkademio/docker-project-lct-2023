18d3217eed2d845b8a9e8075c54975ba0950076e
Kinka <katyamew@gmail.com>
2023-05-26 08:09:35 +1000

    Мде

--- backend-lct-2023/src/controllers/ChildDir.js
+++ backend-lct-2023/src/controllers/ChildDir.js
@@ -1,5 +1,86 @@
 const genericCrud = require('./GenericController')
 const {ChildDirection} = require('../models');
+const {Direction} = require('../models');
 module.exports ={
-    ...genericCrud(ChildDirection),
+     /**
+     * Получение одной записи по id
+     * @param {*} param0 
+     * @param {*} res 
+     * @returns 
+     */
+     async get({params: {id}}, res) {
+        try {
+            const item = await ChildDirection.findById(id)
+            item['parentColor'] = Direction.findById(item.parent).color;
+            return res.status(200).send(item)
+
+        } catch (err) {
+            return res.status(400).send({status: false, err: boom.boomify(err)});
+        }
+    },
+    /**
+     * Получение всех записей
+     * @param {*} _ 
+     * @param {*} res 
+     * @returns 
+     */
+    async getAll(_, res) {
+        try {
+            const items = await ChildDirection.find();
+            items.forEach(item => {
+                item['parentColor'] = Direction.findById(item.parent).color;
+            });
+            return res.status(200).send(items)
+
+        } catch (err) {
+            return res.status(400).send({status: false, err: boom.boomify(err)});
+        }
+    },
+    /**
+     * Добавление записи
+     * @param {*} req 
+     * @param {*} res 
+     * @returns 
+     */
+    async create(req, res) {
+        try {
+
+            const item = new ChildDirection(req.body)
+            const newItem = await item.save()
+            return res.status(200).send(newItem)
+
+
+        } catch (err) {
+            return res.status(400).send({status: false, err: boom.boomify(err)});
+        }
+    },
+    /**
+     * Обновление записи
+     * @param {*} param0 
+     * @param {*} res 
+     * @returns 
+     */
+    async update({params: {id}, body}, res) {
+        try {
+            const item = await ChildDirection.findByIdAndUpdate(id, body, {new: true})
+            return res.status(200).send(item)
+
+        } catch (err) {
+            return res.status(400).send({status: false, err: boom.boomify(err)});
+        }
+    },
+    /**
+     * Удаление записи
+     * @param {*} param0 
+     * @param {*} res 
+     * @returns 
+     */
+    async delete({params: {id}}, res) {
+        try {
+            await ChildDirection.findByIdAndDelete(id)
+            return res.status(200).send({status: 'ok', massage: 'Удалено'})
+        } catch (err) {
+            return res.status(400).send({status: false, err: boom.boomify(err)});
+        }
+    }
 }
--- clients/src/components/Direction.vue
+++ clients/src/components/Direction.vue
@@ -112,18 +112,18 @@
 
       <div class="row">
         <div class="col-3">
-          <q-input style="padding-bottom: 0px" bottom-slots borderless v-model="text" label="Поиск">
+          <q-input style="padding-bottom: 0px" bottom-slots borderless v-model="textCh" label="Поиск">
             <template v-slot:prepend>
               <q-icon name="search"></q-icon>
             </template>
           </q-input>
         </div>
         <div class="col-2">
-          <q-select borderless v-model="searchSelected" :options="getSearchParamsArray" />
+          <q-select borderless v-model="searchSelected" :options="getSearchParamsArrayCh" />
         </div>
         <div class="col-1 q-mt-sm">
-          <q-btn flat icon="add" @click="icon = true">
-            <q-dialog v-model="icon">
+          <q-btn flat icon="add" @click="iconCh = true">
+            <q-dialog v-model="iconCh">
               <q-card class="bg-white text-black add-fact">
                 <q-bar>
                   <q-space />
@@ -137,10 +137,10 @@
                 </q-card-section>
 
                 <q-card-section class="q-pt-none">
-                  <q-input v-model="newName" label="Название" />
-                  <q-color v-model="newColor" no-header-tabs no-footer></q-color>
+                  <q-input v-model="newChildName" label="Название" />
+                  <q-select v-model="newChildParentId" emit-value map-options :options="getParentSelectOptions"></q-select>
                   <q-btn class="q-mt-lg" flat style="width: 100%;  background-color: rgba(7, 7, 7, 0.050);"
-                    @click="addTag()">Принять</q-btn>
+                    @click="addChildTag()">Принять</q-btn>
                 </q-card-section>
               </q-card>
             </q-dialog></q-btn>
@@ -152,7 +152,7 @@
 
 
 
-      <q-table flat borderless separator="cell" :rows="getRows" :columns="columns" row-key="name"
+      <q-table flat borderless separator="cell" :rows="getRowsCh" :columns="columnsCh" row-key="name"
         no-data-label="Ничего не найдено">
         <template v-slot:header-cell="props">
           <q-th :props="props">
@@ -166,21 +166,21 @@
           <q-tr :props="props">
             <q-td key="name" :props="props">
               <div>{{ props.row.name }}</div>
-              <q-popup-edit v-model="props.row.fact_text" @hide="changeTagName(props.row._id, props.row.name)">
+              <q-popup-edit v-model="props.row.fact_text" @hide="changeChildDirName(props.row._id, props.row.name)">
                 <q-input type="textarea" v-model="props.row.name" label="Название тега"></q-input>
               </q-popup-edit>
             </q-td>
 
-            <q-td key="color" :props="props">
+            <q-td key="parent" :props="props">
               <q-chip :style="{'background-color': `${props.row.color}`}"  text-color="white">{{ props.row.name }}
-                <q-popup-edit v-model="props.row.fact_text" @hide="changeTagColor(props.row._id, props.row.color)">
-                  <q-color v-model="props.row.color" no-header-tabs no-footer></q-color>
+                <q-popup-edit v-model="props.row.fact_text" @hide="changeChildDirParent(props.row._id, props.row.parent)">
+                  <q-select v-model="props.row.parent"  emit-value map-options :options="getParentSelectOptions"></q-select>
               </q-popup-edit>
               </q-chip>
             </q-td>
 
             <q-td key="control">
-              <q-btn @click="(confirm = true), (deleteRowId = props.row._id)" flat dense :color="'grey-8'"><q-icon
+              <q-btn @click="(confirm = true), (deleteRowIdCh = props.row._id)" flat dense :color="'grey-8'"><q-icon
                   name="delete_forever" />
                 <q-tooltip>Удалить</q-tooltip></q-btn>
             </q-td>
@@ -202,7 +202,7 @@
 
       <q-card-actions align="between">
         <q-btn flat label="Отмена" color="primary" v-close-popup></q-btn>
-        <q-btn flat label="Удалить" @click="deleteTag(deleteRowId)" color="primary" v-close-popup></q-btn>
+        <q-btn flat label="Удалить" @click="removeChildDir(deleteRowIdCh)" color="primary" v-close-popup></q-btn>
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -217,13 +217,20 @@ export default {
   data() {
     return {
       deleteRowId: -1,
+      deleteRowIdCh: -1,
       confirm: ref(false),
       icon: ref(false),
+      iconCh: ref(false),
       newColor: "#000000",
       newName: "",
+      newChildName: "",
+      newChildParent: "",
+      newChildParentId: "",
       auth: "12GradMapAdmin345SRscx:23pdmtF334slkRDcS5EREc2",
       text: "",
+      textCh: "",
       searchSelected: "",
+      searchSelectedCh: "",
       columns: [
         {
           name: "name",
@@ -246,16 +253,134 @@ export default {
           label: "Управление",
         }
       ],
+      columnsCh: [
+        {
+          name: "name",
+          align: "left",
+          label: "Название",
+          field: "name",
+          canEdit: true,
+          sortable: true
+        },
+        {
+          name: "parent",
+          align: "left",
+          label: "Тег верхнего уровня",
+          canEdit: true,
+          sortable: false
+        },
+        {
+          name: "control",
+          align: "left",
+          label: "Управление",
+        }
+      ],
       rows: [],
+      rowsCh: [],
       error: 0,
       loaded: false,
     };
   },
   mounted() {
     this.getTegs();
+    this.getChildTag();
+    this.searchSelectedCh = this.getSearchParamsArrayCh[0];
     this.searchSelected = this.getSearchParamsArray[0];
   },
   methods: {
+
+    async getChildTag(){
+      try {
+        const res = await api.get("api/childDir/",{
+          headers: {
+            Authorization: "Basic " + btoa(this.auth),
+            "x-requested-with": "*",
+          }
+        });
+        this.rowsCh = res.data;
+      } catch (error) {
+        this.onError(error);
+      }
+    },
+    async addChildTag(){
+      try {
+        const res = await api.post("api/childDir/", {
+          name: this.newChildName,
+          parent: this.newChildParentId
+        },{
+          headers: {
+            Authorization: "Basic " + btoa(this.auth),
+            "x-requested-with": "*",
+          }
+        });
+        this.$q.notify({
+          type: "positive",
+          message: "Тег добавлен",
+        });
+        this.newChildName = "";
+        this.newChildParent = "";
+        this.newChildParentId = "";
+        this.getChildTag();
+      } catch (error) {
+        this.onError(error);
+      }
+    },
+    async removeChildDir(id){
+      try {
+        const res = await api.delete("api/childDir/"+id, {},{
+          headers: {
+            Authorization: "Basic " + btoa(this.auth),
+            "x-requested-with": "*",
+          }
+        });
+        this.$q.notify({
+          type: "positive",
+          message: "Тег удален",
+        });
+        this.getChildTag();
+      } catch (error) {
+        this.onError(error);
+      }
+    },
+    async changeChildDirName(id, name){
+      try {
+        const res = await api.put("api/childDir/"+id, {
+          name: name,
+        },{
+          headers: {
+            Authorization: "Basic " + btoa(this.auth),
+            "x-requested-with": "*",
+          }
+        });
+        this.$q.notify({
+          type: "positive",
+          message: "Тег сохранен",
+        });
+        this.getChildTag();
+      } catch (error) {
+        this.onError(error);
+      }
+    },
+    async changeChildDirParent(id, parent_id){
+      try {
+        const res = await api.put("api/childDir/"+id, {
+          parent: parent_id
+        },{
+          headers: {
+            Authorization: "Basic " + btoa(this.auth),
+            "x-requested-with": "*",
+          }
+        });
+        this.$q.notify({
+          type: "positive",
+          message: "Тег сохранен",
+        });
+        this.getChildTag();
+      } catch (error) {
+        this.onError(error);
+      }
+    },
+
     async getTegs() {
       this.loaded = false;
       this.error = 0;
@@ -422,7 +547,67 @@ export default {
         }
       )
       return result;
-    }
+    },
+    getRowsCh() {
+      if (this.textCh !== "") {
+        let text = this.textCh;
+        let result = [];
+        let searchFiled;
+
+        //Определяем по какому полю будм искать
+        this.columnsCh.forEach((element) => {
+          if (element.label == this.searchSelectedCh) {
+            searchFiled = element.field;
+          }
+        });
+
+        let searchFields = searchFiled.split('.');
+        this.rowsCh.map(function (r) {
+          let object = r[searchFields[0]];
+          for(let index = 1; index<searchFields.length; index ++){
+            object = object[searchFields[index]];
+          }
+
+          if (typeof object == "object") {
+            object.forEach((elem) => {
+              if (elem.toString().toLowerCase().includes(text.toString().toLowerCase())) {
+                result.push(r);
+              }
+            });
+          }
+          else{
+            if (object.toString().toLowerCase().includes(text.toString().toLowerCase())) {
+              result.push(r);
+            }
+          }
+
+        });
+        return result;
+      }
+      return this.rowsCh;
+    },
+    getSearchParamsArrayCh() {
+      let result = [];
+      this.columnsCh.forEach(
+        element => {
+          if (element.field) {
+            result.push(element.label);
+          }
+        }
+      )
+      return result;
+    },
+    getParentSelectOptions(){
+      let selectOptions = [];
+      this.rows.forEach(el=>{
+        selectOptions.push(
+          {label: el.name,
+          value: el._id
+        });
+      })
+      return selectOptions;
+
+    },
 
   },
 }
